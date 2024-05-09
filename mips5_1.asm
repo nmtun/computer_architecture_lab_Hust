9509b8e704acfbc61697ed4e73598493c1cffb4e
Nguyen Manh Tung <163404922+nmtun@users.noreply.github.com>
2024-05-10 01:45:50 +0700

    Delete mips5_1.asm

--- mips5_1.asm
+++
@@ -1,7 +0,0 @@
-#Laboratory Exercise 5, Home Assignment 1
-.data
-test: .asciiz "Nguyen_Manh_Tung_VN07_K67"
-.text
-li $v0, 4	# lựa chọn chức năng	
-la $a0, test	# cập nhật dữ liệu cần in cho $a0
-syscall		# in ra màn hình 
